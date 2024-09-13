9f7db64bc2fe490788dc7e156b48297d3df8ea9f
David Ibanez <omar.ibanez343@gmail.com>
2024-09-12 22:31:14 -0600

    resolved

--- 03_reverseString/reverseString.js
+++ 03_reverseString/reverseString.js
@@ -1,5 +1,11 @@
-const reverseString = function() {
+const reverseString = function(stringToReverse) {
+    let splitString = stringToReverse.split("");
 
+    let reversedArray = splitString.reverse();
+
+    let joinArray = reversedArray.join("");
+
+    return joinArray;
 };
 
 // Do not edit below this line
--- 04_removeFromArray/removeFromArray.js
+++ 04_removeFromArray/removeFromArray.js
@@ -1,4 +1,16 @@
-const removeFromArray = function() {
+const removeFromArray = function(array, elementToRemove) {
+    let newArray = [];
+
+    console.log(elementToRemove);
+
+    array.forEach(element => {
+
+        if (element != elementToRemove) {
+            newArray.push(element);
+        }
+
+    });
+    return newArray;
 };
 
 // Do not edit below this line
